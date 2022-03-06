f494662b01d4f63180de9ead690d7ef79bc825b0
JulienOo <julien.pernet@live.fr>
2022-03-06 20:36:39 +0100

    amélioration

--- WiFi.bat
+++ WiFi.bat
@@ -1,9 +1,25 @@
 @echo off 
+
+REM ###########################################################
+
+
+REM version : 1.0.0.0
+
+REM github : https://github.com/JulienOo/WiFi-configuration
+
+
+REM ###########################################################
+
+
 REM modification gestion variables
 setlocal enabledelayedexpansion 
 
 REM vérification présence repertoire Wifi
 if not exist "./WiFi" ( mkdir WiFi )
+
+REM recherche si un fichier temporaire existe déjà
+if exist "temp.txt" ( del "temp.txt" )
+
 :startApp
 
 REM vide le contenu de l'affichage
@@ -14,13 +30,16 @@ REM actualise le repertoire
 cd %~dp0
 
 
-echo SCRIPT WIFI
+echo SCRIPT Wi-Fi
 :otherChoice
+echo.
+echo.
 echo * * * * * * * * * * * * *
+echo.
 
-chcp 65001 > NUL
-echo 1 - configuration d'un wifi sur l'ordinateur 
-echo 2 - ajout d'un réseau Wifi sur le SCRIPT
+chcp 1252 > NUL
+echo 1 - ajout d'un réseau Wifi sur le SCRIPT
+echo 2 - configuration d'un wifi sur l'ordinateur 
 echo 3 - fermer le script 
 echo.
 
@@ -29,8 +48,8 @@ echo.
 set /p choix= votre choix : 
 
 
-if %choix% EQU 1 ( goto :configurationWifi )
-if %choix% EQU 2 ( goto :ajoutWiFiScript )
+if %choix% EQU 1 ( goto :ajoutWiFiScript )
+if %choix% EQU 2 ( goto :configurationWifi )
 if %choix% EQU 3 ( goto :closeApp )
 
 
@@ -52,6 +71,9 @@ echo.
 echo.
 echo Voici les réseaux wifi disponible : 
 echo.
+echo.
+echo !compteur! - ajouter tous les wifi 
+echo.
 for %%a in ("WiFi/*.xml") do (
 	set /a compteur+=1
 	echo !compteur! - %%a)
@@ -67,7 +89,7 @@ if /i !compteur! EQU 0 (
 	set /p wifi= Quel wifi ajouter ? 
 	echo.
 	if /i !wifi! GTR !compteur! (echo Merci de choisir une valeur correct ! && goto :return2 )
-	if /i !wifi! LSS 1 (echo Merci de choisir une valeur correct ! && goto :return2 )
+	if /i !wifi! LSS 0 (echo Merci de choisir une valeur correct ! && goto :return2 )
 )
 
 
@@ -77,30 +99,21 @@ set /a compteur=0
 for %%a in ("WiFi/*.xml") do (
 	set /a compteur+=1
 
-	if /i !wifi! EQU !compteur! (netsh wlan add profile filename="WiFi/"%%a user=all )
+	if /i !wifi! EQU !compteur! (netsh wlan add profile filename="WiFi/%%a" user=all )
+	if /i !wifi! EQU 0 (netsh wlan add profile filename="WiFi/%%a" user=all )
 )
 echo.
 pause
 goto :startApp
 
 
-rem rem netsh wlan add profile filename="WiFi/Formation-Premica.xml" user=all
-
-
-rem pause
-rem exit
-
 
 
 :ajoutWiFiScript
 netsh wlan show profiles > temp.txt
 
-
 echo > temp2.txt
-
-
 more +9 temp.txt > temp2.txt
-
 del temp.txt
 ren temp2.txt temp.txt
 
@@ -109,8 +122,12 @@ echo.
 echo.
 echo Voici les Wifi configuré sur l'ordinateur
 echo.
-
+echo.
 set /a compteur=0
+echo !compteur! - ajouter tous les wifi !
+echo.
+
+
 for /f "delims=" %%i in ('type temp.txt') do (
     set var=%%i
 
@@ -120,8 +137,7 @@ for /f "delims=" %%i in ('type temp.txt') do (
 	echo !compteur! - !var:~39!
     )
 	set /a compteur+=1
-	echo !compteur! - ajouter tous les wifi !
-
+	
 
 del temp.txt
 ren temp2.txt temp.txt
@@ -134,16 +150,18 @@ set /p ajoutWifi= Quel wifi ajouter ?
 
 
 
-set /a compteur=0
+set /a compteur=1
+
+if /i !ajoutWifi! EQU 0 (cd "%~dp0\Wifi" && netsh wlan export profile key=clear folder=. && goto :cestgood  )
 for /f "delims=" %%i in ('type temp.txt') do (
-	set /a compteur+=1
     set var=%%i
-
-	if /i !ajoutWifi! EQU !compteur! (cd %~dp0"\Wifi" && netsh wlan export profile !var! key=clear folder=. && goto :cestgood )
-
-    )
+echo %%i
+echo !compteur!
+	if /i !ajoutWifi! EQU !compteur! (cd "%~dp0\Wifi" && netsh wlan export profile !var! key=clear folder=. && goto :cestgood )
 	set /a compteur+=1
-	if /i !ajoutWifi! EQU !compteur! (cd %~dp0"\Wifi" && netsh wlan export profile key=clear folder=. && goto :cestgood  )
+    )
+
+	set /a compteur+=1
 
 echo.
 echo Merci de choisir une valeur correct ! && goto :return3
